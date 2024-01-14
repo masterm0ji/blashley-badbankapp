53a3150680615006dcf051c8602bb6ef48f4c62a
B <117953626+masterm0ji@users.noreply.github.com>
2024-01-14 07:26:47 -0500

    Update alldata.js

--- src/alldata.js
+++ src/alldata.js
@@ -1,34 +1,33 @@
-function AllData(){
-  const users = [
-    {email: 'dom@me.com', name: 'Dom', password: 'Access123'},
-    {email: 'blashley@school.edu', name: 'Brandy', password: 'Passcode321'},
-    {email: 'mochalla@catz.com', name: 'Mocha', password: 'CatzRule!'},
-  ]
+function AllData() {
+  const ctx = React.useContext(UserContext);
+
   return (
     <>
-    <h1>All Data</h1>
-    <table style={{border: '1px solid black', width: '100%'}}>
-      <thead>
-        <tr>
-          <th style={{backgroundColor: '#fd7e14'}}>Email</th>
-          <th style={{backgroundColor: '#fd7e14'}}>Name</th>
-          <th style={{backgroundColor: '#fd7e14'}}>Password</th>
-        </tr>
-      </thead>
-      <tbody>
-        { 
-          users.map((user, index) => {
-            return (
-              <tr key={index}>
-                <td>{user.email}</td>
-                <td>{user.name}</td>
-                <td>{user.password}</td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+      <h1>All Data</h1>
+      <table style={{border: '1px solid black', width: '100%'}}>
+        <thead>
+          <tr>
+            <th style={{backgroundColor: '#fd7e14'}}>Email</th>
+            <th style={{backgroundColor: '#fd7e14'}}>Name</th>
+            <th style={{backgroundColor: '#fd7e14'}}>Password</th>
+            <th style={{backgroundColor: '#fd7e14'}}>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+            ctx.users.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.email}</td>
+                  <td>{user.name}</td>
+                  <td>{user.password}</td>
+                  <td>{user.balance}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </>
   );
 }
